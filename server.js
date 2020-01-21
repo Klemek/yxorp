4446d7471f331bfd2e7c59d8d2fcbd94fb88b274
klemek <klemek@outlook.fr>
2020-01-21 21:00:01 +0100

    more stuff

--- server.js
+++ server.js
@@ -17,7 +17,89 @@ const DEBUG = {
   INIT_REQ: 256,
 };
 
-const DEBUG_LEVEL = DEBUG.REQUEST;
+const DEBUG_LEVEL = DEBUG.REQUEST | DEBUG.RESPONSE;
+
+const PROTOCOLS = {
+  'acap:': 674,
+  'afp:': 548,
+  'dict:': 2628,
+  'dns:': 53,
+  'ftp:': 21,
+  'git:': 9418,
+  'gopher:': 70,
+  'http:': 80,
+  'https:': 443,
+  'imap:': 143,
+  'ipp:': 631,
+  'ipps:': 631,
+  'irc:': 194,
+  'ircs:': 6697,
+  'ldap:': 389,
+  'ldaps:': 636,
+  'mms:': 1755,
+  'msrp:': 2855,
+  'mtqp:': 1038,
+  'nfs:': 111,
+  'nntp:': 119,
+  'nntps:': 563,
+  'pop:': 110,
+  'prospero:': 1525,
+  'redis:': 6379,
+  'rsync:': 873,
+  'rtsp:': 554,
+  'rtsps:': 322,
+  'rtspu:': 5005,
+  'sftp:': 22,
+  'smb:': 445,
+  'snmp:': 161,
+  'ssh:': 22,
+  'svn:': 3690,
+  'telnet:': 23,
+  'ventrilo:': 3784,
+  'vnc:': 5900,
+  'wais:': 210,
+  'ws:': 80,
+  'wss': 443
+};
+
+const REVERSE_PROTOCOLS = {
+  674: 'acap:',
+  548: 'afp:',
+  2628: 'dict:',
+  53: 'dns:',
+  21: 'ftp:',
+  9418: 'git:',
+  70: 'gopher:',
+  80: 'http:',
+  443: 'https:',
+  143: 'imap:',
+  631: 'ipp:',
+  194: 'irc:',
+  6697: 'ircs:',
+  389: 'ldap:',
+  636: 'ldaps:',
+  1755: 'mms:',
+  2855: 'msrp:',
+  1038: 'mtqp:',
+  111: 'nfs:',
+  119: 'nntp:',
+  563: 'nntps:',
+  110: 'pop:',
+  1525: 'prospero:',
+  6379: 'redis:',
+  873: 'rsync:',
+  554: 'rtsp:',
+  322: 'rtsps:',
+  5005: 'rtspu:',
+  22: 'sftp:',
+  445: 'smb:',
+  161: 'snmp:',
+  3690: 'svn:',
+  23: 'telnet:',
+  3784: 'ventrilo:',
+  5900: 'vnc:',
+  210: 'wais:'
+};
 
 const REMOVE_REQ_HEADERS = [
   'accept-encoding',
@@ -38,17 +120,28 @@ Object.keys(DEBUG).forEach(key => {
 });
 
 const port = process.argv[2] || 5050;
-let proxy = process.argv[3] || `http://localhost:${port}`;
-const proxyHost = url.parse(proxy).host;
+const proxyPart = process.argv[3] || `http://localhost:${port}`;
+const proxy = url.parse(proxyPart);
 const index = process.argv[4] || 'index.html';
 const sourceHistory = {};
 
 console.log('Loading home page...');
 const html = fs.readFileSync(index);
 
-if (!proxy.endsWith('/'))
-  proxy += '/';
 
+/**
+ * Extract port and hostname to redirect correctly
+ * @param parsedUrl
+ * @returns {string}
+ */
+const writeHost = (parsedUrl) => {
+  if (parsedUrl.port)
+    return '/' + parsedUrl.port + '/' + parsedUrl.hostname;
+  else if (parsedUrl.protocol !== proxy.protocol)
+    return '/' + PROTOCOLS[parsedUrl.protocol] + '/' + parsedUrl.hostname;
+  else
+    return '/' + parsedUrl.hostname;
+};
 
 /**
  * Rewrite URL to add proxy before it
@@ -59,17 +152,17 @@ if (!proxy.endsWith('/'))
  * @returns {string}
  */
 const rewriteUrl = (prefix, u, suffix, targetUrl) => {
-  if (u.includes(proxy)) //already treated
+  if (u.includes(proxy.hostname)) //already treated
     return prefix + u + suffix;
-  let parsedUrl = url.parse(targetUrl);
+  let parsedTarget = url.parse(targetUrl);
   let parsedMatch = url.parse(u);
-  let targetOrigin = `${parsedUrl.protocol}//${parsedUrl.host}`;
-  if (parsedMatch.protocol) { // full URL with protocol (https://google.com/favicon.ico)
-    return prefix + proxy + parsedMatch.host + parsedMatch.path + suffix; // simply add proxy
+  if (parsedMatch.protocol && parsedMatch.slashes) { // full URL with protocol (https://google.com/favicon.ico)
+    return prefix + proxyPart + writeHost(parsedMatch) + parsedMatch.path + suffix; // simply add proxy
   } else if (u.startsWith('//')) { // full URL without protocol (//google.com/favicon.ico)
-    return prefix + proxy + u.substr(2) + suffix; // add proxy
+    parsedMatch = url.parse('http:' + u);
+    return prefix + proxy.protocol + '//' + proxy.host + writeHost(parsedMatch) + suffix; // add proxy
   } else if (u.startsWith('/')) { // URL with root path (/favicon.ico)
-    return prefix + proxy + targetOrigin + u + suffix; // add proxy and origin (https://google.com)
+    return prefix + parsedTarget.protocol + '//' + proxy.host + writeHost(parsedTarget) + u + suffix; // add proxy and origin (https://google.com)
   } else { // relative path (favicon.ico)
     return prefix + u + suffix; // keep it that way
   }
@@ -150,13 +243,15 @@ const cssTransform = (targetUrl) => contentTransform(input => changeByRegex(inpu
  * @param {string} targetHost - current page Host
  * @returns {module:stream.internal.Transform}
  */
-const scriptTransform = (targetHost) => contentTransform(input => {
+const scriptTransform = () => contentTransform(input => {
   // found domains like (//something.com/)
   let output1 = changeByRegex(input, /\/\/((\w+\.)+\w+)\//gm,
-    m => '//' + proxyHost + m[0].substr(1), DEBUG.SCRIPT_MATCH);
+    m => '//' + proxy.host + m[0].substr(1), DEBUG.SCRIPT_MATCH);
   // found escaped domains like (\/\/something.com\/)
-  return changeByRegex(output1, /\\\/\\\/((\w+\.)+\w+)\\\//gm,
-    m => '\\/\\/' + proxyHost + m[0].substr(2), DEBUG.SCRIPT_MATCH);
+  let output2 = changeByRegex(output1, /\\\/\\\/((\w+\.)+\w+)\\\//gm,
+    m => '\\/\\/' + proxy.host + m[0].substr(2), DEBUG.SCRIPT_MATCH);
+  return changeByRegex(output2, /\/\/# sourceMappingURL=[^\n]+/gm,
+    m => '', DEBUG.NONE);
 });
 
 /**
@@ -164,38 +259,46 @@ const scriptTransform = (targetHost) => contentTransform(input => {
  * @param {string} targetUrl - current page URL
  * @returns {module:stream.internal.Transform}
  */
-const basicTransform = (targetUrl) => contentTransform(input => changeByRegex(input, /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gm,
-  m => rewriteUrl('', m[0], '', targetUrl), DEBUG.BASIC_MATCH));
+const basicTransform = (targetUrl) =>
+  contentTransform(input => {
+    // full URLs
+    let output1 = changeByRegex(input, /[a-z]{2,}:\/\/([\w_-]+((\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/gm,
+      m => rewriteUrl('', m[0], '', targetUrl), DEBUG.BASIC_MATCH);
+    // full escaped URLs
+    return changeByRegex(input, /[a-z]{2,}:\\\/\\\/([\w_-]+((\.[\w_-]+)+))(([\w.,@?^=%&:~+#-]|\\\/)*([\w@?^=%&~+#-]|\\\/))?/gm,
+      m => rewriteUrl('', m[0].replace(/\\\//g, '/'), '', targetUrl).replace(/\//g, '\\/'), DEBUG.BASIC_MATCH);
+  });
 
 /**
  * Core of the proxy, will send the request and compute the result
  * @param req
  * @param res
+ * @param {number} reqPort
  */
-const proxyRequest = (req, res) => {
+const proxyRequest = (req, res, reqPort) => {
   const source = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const time = new Date().getTime();
   // clear history if too old
   if (sourceHistory[source] && time - sourceHistory[source].time > HISTORY_TIMEOUT)
     delete sourceHistory[source];
 
-  const reqUrl = url.parse(req.url); // keep requested URL
+  let reqUrl = url.parse(req.url); // keep requested URL
 
   const reqHost = req.headers['host'];
-  if (sourceHistory[source] && reqHost && reqHost.endsWith(proxyHost) && reqHost.length > proxyHost.length) {
+  if (sourceHistory[source] && reqHost && reqHost.endsWith(proxy.host) && reqHost.length > proxy.host.length) {
     // something is before the host
-    const subHost = reqHost.substr(0, reqHost.length - proxyHost.length);
+    const subHost = reqHost.substr(0, reqHost.length - proxy.host.length);
     if (DEBUG_LEVEL & DEBUG.REDIRECT)
       console.log(req.url + ' => ' + subHost + sourceHistory[source].host + '/' + reqUrl.path);
     req.url = subHost + sourceHistory[source].host + '/' + reqUrl.path;
   }
 
-  req.url = 'https://' + req.url;
-  const targetHost = url.parse(req.url).host; // extract target host
+  req.url = (REVERSE_PROTOCOLS[reqPort] || 'https:') + '//' + req.url;
+  reqUrl = url.parse(req.url);
 
   const onError = () => {
-    // targetHost is last request, normal error
-    if (!sourceHistory[source] || sourceHistory[source].host === targetHost) {
+    // target host is last request, normal error
+    if (!sourceHistory[source] || sourceHistory[source].host === reqUrl.host) {
       if (DEBUG_LEVEL & DEBUG.ERROR)
         console.error(`${source}!>${req.method} ${req.url}`);
       res.writeHead(502, `cannot access ${req.url}`, {"Content-Type": "text/plain"});
@@ -211,7 +314,7 @@ const proxyRequest = (req, res) => {
   try {
     if (req.headers['upgrade-insecure-requests'] || !sourceHistory[source]) {
       sourceHistory[source] = {
-        host: targetHost,
+        host: reqUrl.host,
         time: time
       };
       if (DEBUG_LEVEL & (DEBUG.REQUEST | DEBUG.INIT_REQ))
@@ -219,7 +322,7 @@ const proxyRequest = (req, res) => {
     } else if (DEBUG_LEVEL & DEBUG.REQUEST)
       console.log(`${source}>${req.method} ${req.url}`);
     // change request headers to avoid issues
-    req.headers['host'] = targetHost;
+    req.headers['host'] = reqUrl.host;
     REMOVE_REQ_HEADERS.forEach(key => delete req.headers[key]);
     // pipe original request to a new one
     req.pipe(request(req.url)
@@ -237,7 +340,7 @@ const proxyRequest = (req, res) => {
         // change data by content-type
         switch (contentType) {
           case 'text/html':
-            r.pipe(scriptTransform(targetHost))
+            r.pipe(scriptTransform())
               .pipe(htmlTransform(req.url))
               .pipe(cssTransform(req.url))
               .pipe(basicTransform(req.url))
@@ -250,8 +353,7 @@ const proxyRequest = (req, res) => {
             break;
           case 'text/javascript':
           case 'application/javascript':
-            // TODO breaking some JS, need to refine
-            r.pipe(scriptTransform(targetHost))
+            r.pipe(scriptTransform())
               .pipe(basicTransform(req.url))
               .pipe(res);
             break;
@@ -289,18 +391,25 @@ const server = http.createServer((req, res) => {
     res.end();
   } else { // redirect to URL
     req.url = req.url.substr(1); // remove initial / in path
+    let reqPort;
+    if (/^\d+\//gm.test(req.url)) {
+      reqPort = req.url.split('/')[0];
+      req.url = req.url.substr(reqPort.length + 1);
+      reqPort = parseInt(reqPort);
+    }
     const reqUrl = url.parse(req.url);
+    reqPort = reqPort || reqUrl.port || PROTOCOLS[reqUrl.protocol] || PROTOCOLS[proxy.protocol];
     if (reqUrl.protocol != null) {
-      res.writeHead(301, {"Location": reqUrl.slashes ? '/' + reqUrl.host + reqUrl.path : reqUrl.path});
+      res.writeHead(302, {"Location": reqUrl.slashes ? '/' + reqUrl.host + reqUrl.path : reqUrl.path});
       res.end();
     } else if (/^(\w+\.)+\w+$/g.test(req.url)) {
-      res.writeHead(301, {"Location": '/' + req.url + '/'});
+      res.writeHead(302, {"Location": '/' + req.url + '/'});
       res.end();
     } else {
-      proxyRequest(req, res);
+      proxyRequest(req, res, reqPort);
     }
   }
 });
 
-console.log(`yxorp started at ${proxy}`);
+console.log(`yxorp started at ${proxyPart}`);
 server.listen(port);
