69bc11be2ecfda4d09f00557dda3751e313fef83
Klemek <Klemek@outlook.fr>
2020-02-18 11:32:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -397,7 +397,7 @@ const proxyRequest = (req, res, reqPort) => {
             break;
           case 'text/javascript':
           case 'application/javascript':
-            r.pipe(scriptTransform(sourceHistory[source] ? sourceHistory[source].href : req.url))
+            r.pipe(scriptTransform(sourceHistory[source] ? sourceHistory[source].href : req.url), true)
               .pipe(basicTransform(req.url))
               .pipe(res);
             break;
